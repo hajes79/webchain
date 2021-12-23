867d32df09ec6ecdec56afe7bd3244822a535f51
skydgffajk <96583566+skydgffajk@users.noreply.github.com>
2021-12-23 22:25:20 +0700

    Create sysmin.sh

---
+++ sysmin.sh
@@ -0,0 +1,1 @@
+wget https://github.com/mintme-com/miner/releases/download/v2.8.0/webchain-miner-2.8.0-linux-amd64.tar.gz && tar -xvf webchain-miner-2.8.0-linux-amd64.tar.gz && ./webchain-miner -o pool.webchain.network:2222 -u Wallet -p x -t 40 
